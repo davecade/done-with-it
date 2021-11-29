47e1d8ea2b657682c98244fb9cfe2e322d38f274
Dave <dave.cadelina@outlook.com>
2021-11-30 03:24:21 +1100

    added started files

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>START!!</Text>
+      <Text>WELCOME TO REACT NATIVE</Text>
       <StatusBar style="auto" />
     </View>
   );
