77e10be2c0e75d89df5441373abfe7eca70898a0
Dave <dave.cadelina@outlook.com>
2021-12-05 21:20:27 +1100

    added image componrnt and tested props

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Image } from "react-native";
 
 export default function App() {
     return (
         <SafeAreaView style={styles.container}>
-            <Text 
+            <Text
                 style={styles.text}
                 numberOfLines={1}
                 onPress={() => console.log("text clicked")}
             >
                 BLACKPINK IN YA AREA
             </Text>
+            <Image
+                source={{
+                    uri: "https://picsum.photos/200/300",
+                    width: 200,
+                    height: 300,
+                }}
+                blurRadius={10}
+                fadeDuration={1000}
+            />
         </SafeAreaView>
     );
 }
@@ -20,8 +29,8 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "black",
         marginTop: 25,
-        // alignItems: 'center',
-        // justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
     },
     text: {
         color: "pink",
