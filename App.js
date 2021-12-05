c72c84af2fd2a7b4f9c403de9f380d82c93cddf1
Dave <dave.cadelina@outlook.com>
2021-12-05 20:46:48 +1100

    added text and added some props

--- App.js
+++ App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, SafeAreaView } from "react-native";
 
 export default function App() {
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>BLACKPINK IN YA AREA</Text>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text 
+                style={styles.text}
+                numberOfLines={1}
+                onPress={() => console.log("text clicked")}
+            >
+                BLACKPINK IN YA AREA
+            </Text>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'dodgerblue',
-    padding: "20 10"
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  text: {
-    color: "pink",
-    fontWeight: "bold"
-  }
+    container: {
+        flex: 1,
+        backgroundColor: "black",
+        marginTop: 25,
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    text: {
+        color: "pink",
+        fontWeight: "bold",
+    },
 });
