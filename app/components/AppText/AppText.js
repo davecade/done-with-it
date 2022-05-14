2c34fef85006557873096e2eef893ea34af4b74f
Dave <dave.cadelina@outlook.com>
2022-05-15 09:06:39 +1000

    react style styles

--- app/components/AppText/AppText.js
+++ app/components/AppText/AppText.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Text, StyleSheet, Platform } from "react-native";
+import { Text } from "react-native";
+import styles from "./styles";
 
 const AppText = ({ children }) => {
     return <Text style={styles.text}>{children}</Text>;
